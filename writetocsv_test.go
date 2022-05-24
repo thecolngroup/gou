8b4208d3a400195c0988f42c70fb1b2f5741d5f6
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-24 10:30:52 +0100

    test: write to csv test

---
+++ writetocsv_test.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MyStruct struct {
+	Name       string
+	Properties map[string]any
+}
+
+func TestWriteToCSV(t *testing.T) {
+
+	data := []MyStruct{
+		{
+			Name:       "Row 1",
+			Properties: map[string]any{"Param 1": "0"},
+		},
+		{
+			Name:       "Row 2",
+			Properties: map[string]any{"Param 1": 0},
+		},
+	}
+
+	encMap := func(m map[string]any) ([]byte, error) {
+		return []byte(fmt.Sprint(m)), nil
+	}
+
+	err := WriteToCSV("testdata/writetocsv_test.csv", data, encMap)
+	assert.NoError(t, err)
+}
